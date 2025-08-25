322dcc7eafe299ecab0047d85d408f99736dc33f
kevinstifel <krstifel@uc.cl>
2025-08-25 15:33:36 -0400

    initial

---
+++ src/DCControl/main.c
@@ -0,0 +1,43 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../input_manager/manager.h"
+
+int main(int argc, char const *argv[]) {
+    set_buffer();  // ya dado, no lo toques
+    int time_max = (argc > 1) ? atoi(argv[1]) : -1; // -1 si no hay límite
+
+    while (1) {
+        char **input = read_user_input();
+
+        if (strcmp(input[0], "launch") == 0) {
+            // handle_launch(input, time_max);
+            printf("[DEBUG] Launch command received\n");
+
+        } else if (strcmp(input[0], "status") == 0) {
+            // handle_status();
+            printf("[DEBUG] Status command received\n");
+
+        } else if (strcmp(input[0], "abort") == 0) {
+            // handle_abort(atoi(input[1]));
+            printf("[DEBUG] Abort command received\n");
+
+        } else if (strcmp(input[0], "shutdown") == 0) {
+            // handle_shutdown();
+            printf("[DEBUG] Shutdown command received\n");
+            break;
+
+        } else if (strcmp(input[0], "emergency") == 0) {
+            // handle_emergency();
+            printf("[DEBUG] Emergency command received\n");
+            break;
+        }
+
+        free_user_input(input);
+    }
+
+    return 0;
+}
+
+
